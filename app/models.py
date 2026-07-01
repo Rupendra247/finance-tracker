bf52e8eaac7de76c69879afd8d95aa45f61ee6b2
Rupendra Dhungana <dhunganarupendra03@gmail.com>
2026-07-01 18:24:22 +0545

    make changes in models.py added foreignkeyand and relationship

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.database import Base
 
@@ -9,3 +10,15 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
+    transactions = relationship("Transaction", back_populates="owner")
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    description = Column(String)
+    type = Column(String)
+    date = Column(DateTime, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="transactions")
