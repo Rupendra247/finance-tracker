a33c6868f84deca7057b9867fc544b8ecbb5796c
Rupendra Dhungana <dhunganarupendra03@gmail.com>
2026-07-11 13:27:51 +0545

    solve fastapi 0auth2

--- app/routers.py
+++ app/routers.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app import models, schemas
@@ -26,11 +27,11 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return new_user
 
 @router.post("/login")
-def login(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == form_data.username).first()
     if not db_user:
         raise HTTPException(status_code=404, detail="User not found")
-    if not verify_password(user.password, db_user.password):
+    if not verify_password(form_data.password, db_user.password):
         raise HTTPException(status_code=400, detail="Incorrect password")
     token = create_access_token(data={"sub": db_user.email})
     return {"access_token": token, "token_type": "bearer"}
@@ -41,7 +42,7 @@ def create_transaction(transaction: schemas.TransactionCreate, db: Session = Dep
         amount=transaction.amount,
         description=transaction.description,
         type=transaction.type,
-        user_id= current_user.id
+        user_id=current_user.id
     )
     db.add(new_transaction)
     db.commit()
